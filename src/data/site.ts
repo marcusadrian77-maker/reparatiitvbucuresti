d4e2f41074835b422167d3fb7e6e04e712281889
marcusadrian77-maker <marcusadrian77@gmail.com>
2026-06-14 19:41:48 +0300

    Update site.ts

--- src/data/site.ts
+++ src/data/site.ts
@@ -32,6 +32,15 @@ export const NAV_SECTOARE = [
   { href: u('reparatii-televizoare-sector-6/'), label: 'Sector 6' },
 ];
 
+export const STRAZI_PRINCIPALE: Record<string, string[]> = {
+  '1': ['Calea Victoriei','Bulevardul Aviatorilor','Bulevardul Ion Mihalache','Calea Dorobanți','Bulevardul Lascăr Catargiu','Șoseaua Kiseleff','Bulevardul Banu Manta','Calea Griviței','Șoseaua București-Ploiești','Bulevardul Poligrafiei'],
+  '2': ['Bulevardul Lacul Tei','Șoseaua Colentina','Șoseaua Ștefan cel Mare','Bulevardul Ferdinand I','Bulevardul Basarabia','Șoseaua Pantelimon','Șoseaua Mihai Bravu','Bulevardul Chișinău','Calea Moșilor','Bulevardul Pache Protopopescu'],
+  '3': ['Bulevardul Unirii','Bulevardul Decebal','Bulevardul Camil Ressu','Bulevardul Nicolae Grigorescu','Calea Vitan','Calea Călărașilor','Bulevardul Theodor Pallady','Bulevardul 1 Decembrie 1918','Strada Matei Basarab','Bulevardul Burebista'],
+  '4': ['Bulevardul Tineretului','Bulevardul Dimitrie Cantemir','Șoseaua Olteniței','Șoseaua Berceni','Bulevardul Gheorghe Șincai','Calea Văcărești','Bulevardul Constantin Brâncoveanu','Bulevardul Metalurgiei','Bulevardul Alexandru Obregia'],
+  '5': ['Calea Rahovei','Calea 13 Septembrie','Șoseaua Panduri','Bulevardul Tudor Vladimirescu','Calea Ferentari','Bulevardul Eroii Sanitari','Strada Sebastian','Bulevardul Pieptănari','Șoseaua Alexandriei','Șoseaua Sălaj'],
+  '6': ['Bulevardul Iuliu Maniu','Bulevardul Timișoara','Drumul Taberei','Calea Crângași','Calea Giulești','Bulevardul Ghencea','Șoseaua Virtuții','Bulevardul Constructorilor','Bulevardul Preciziei','Bulevardul Uverturii'],
+};
+
 export const SCHEMA_LOCALBUSINESS = {
   '@context': 'https://schema.org',
   '@type': 'ElectronicsStore',
